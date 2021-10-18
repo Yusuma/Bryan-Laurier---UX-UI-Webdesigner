63fa01dcf37798d7e3018c6729bdb0f8d108df4c
Bryan Laurier <36500785+Yusuma@users.noreply.github.com>
2021-10-18 19:18:05 +0200

    pop up solved part 2 ?

--- js/script.js
+++ js/script.js
@@ -63,38 +63,38 @@ tabsContainer.addEventListener("click", (e) => {
     }
 });
 
-/*-------------- portoflio Item Details Popup ----------------*/
+/*-------------- portfolio Item Details Popup ----------------*/
 document.addEventListener("click", (e) => {
     if (e.target.classList.contains("view-project-btn")) {
-        toggleportoflioPopup();
-        document.querySelector(".portoflio-popup").scrollTo(0, 0);
-        portoflioItemDetails(e.target.parentElement);
+        toggleportfolioPopup();
+        document.querySelector(".portfolio-popup").scrollTo(0, 0);
+        portfolioItemDetails(e.target.parentElement);
     }
 })
 
-function toggleportoflioPopup() {
+function toggleportfolioPopup() {
     document.querySelector(".portfolio-popup").classList.toggle("open");
     document.body.classList.toggle("hide-scrolling");
     document.querySelector(".main").classList.toggle("fade-out");
 }
 
-document.querySelector(".pp-close").addEventListener("click", toggleportoflioPopup);
+document.querySelector(".pp-close").addEventListener("click", toggleportfolioPopup);
 
 //hide popup when clicking outside of it
 document.addEventListener("click", (e) => {
     if (e.target.classList.contains("pp-inner")) {
-        toggleportoflioPopup();
+        toggleportfolioPopup();
     }
 });
 
-function portoflioItemDetails(portoflioItem) {
+function portfolioItemDetails(portfolioItem) {
     document.querySelector(".pp-thumbnail img").src =
-        portoflioItem.querySelector(".portoflio-item-thumbnail img").src;
+        portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
 
     document.querySelector(".pp-header h3").innerHTML =
-        portoflioItem.querySelector(".portoflio-item-title").innerHTML;
+        portfolioItem.querySelector(".portfolio-item-title").innerHTML;
 
     document.querySelector(".pp-body").innerHTML =
-        portoflioItem.querySelector(".portoflio-item-details").innerHTML;
+        portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 
 }
